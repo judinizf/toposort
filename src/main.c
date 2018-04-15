05a58eadbc6750e9e5cd82d032c1b2c4988d5b1b
Julia <judinizf@gmail.com>
2018-04-15 18:35:06 -0300

    Parte 1 finalizada - main com selection

--- src/main.c
+++ src/main.c
@@ -9,27 +9,26 @@
 #include "queue.h"
 
 int main (int argc, char *argv[]) {
-	int wc, nvert, narest, i;
+	int op, nvert, narest, i;
 	int n, k, vol;
 	vertice v1,v2, w;
 
-	// PARTE 1
-	scanf("%d", &wc);
-	scanf("%d %d", &nvert, &narest);
-	
-	grafo g = inigrafo(nvert);
-	pilha p = {0, -1, NULL};
-	
-	for (i = 0; i < narest; i++) {
-		scanf("%d %d", &v1, &v2);
-		addarco(&g,(v1-1),(v2-1));
+	scanf("%d", &op);
+	if(op == 1){
+		// PARTE 1
+		scanf("%d %d", &nvert, &narest);
+		grafo g = inigrafo(nvert);
+		pilha p = {0, -1, NULL};
+		for (i = 0; i < narest; i++) {
+			scanf("%d %d", &v1, &v2);
+			addarco(&g,(v1-1),(v2-1));
+		}
+		ordenacaotopologica(&g);	
+	}else{
+		// PARTE 2
+		scanf("%d %d %d", &n, &k, &vol);
+		//addontop(&p, n);
 	}
-	ordenacaotopologica(&g);
-	
-	// PARTE 2
-	scanf("%d", &wc);
-	scanf("%d %d %d", &n, &k, &vol);
-	//addontop(&p, n);
 	
 	return 0;
 }
